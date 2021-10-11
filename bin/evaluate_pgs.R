1cbf7c857cd5fec33987bc23820fe26cf95f48d6
Vivek Appadurai <vapp0002@vivekmbpm1work.regionh.top.local>
2021-10-11 12:50:07 +0200

    function modifications

--- bin/evaluate_pgs.R
+++ bin/evaluate_pgs.R
@@ -77,20 +77,22 @@ sum_scale_scores = function (x_df, score_col_name) {
 
 calculate_r2_p = function(x_df, y_df, binary) {
     colnames(y_df) = c("IID", "SCORE")
+    head(x_df)
+    head(y_df)
     pgs            = inner_join(x_df, y_df, by = c("IID"))
     r2             = 0
     p              = 0
     
     if(isTRUE(binary)) {
-        fit_null = glm(data = pgs, Pheno ~ . -IID)
-        fit_bin  = glm(data = pgs, Pheno ~ . -IID -SCORE)
+        fit_null = glm(data = pgs, Pheno ~ . -IID -SCORE)
+        fit_bin  = glm(data = pgs, Pheno ~ . -IID)
         r2       = NagelkerkeR2(fit_bin) - NagelkerkeR2(fit_null)
         p        = pchisq(deviance(fit_null) - deviance(fit_bin),
                           df.residual(fit_null) - df.residual(fit_bin),
                           lower.tail = F) 
     } else {
-        fit_null = lm(data = x_df, Pheno ~ . -IID)
-        fit_con  = lm(data = pgs, Pheno ~ . -IID -SCORE)
+        fit_null = lm(data = pgs, Pheno ~ . -IID -SCORE)
+        fit_con  = lm(data = pgs, Pheno ~ . -IID)
         r2       = summary(fit_con)$r.squared - summary(fit_null)$r.squared
         p        = pchisq(deviance(fit_null) - deviance(fit_con),
                           df.residual(fit_null) - df.residual(fit_con),
