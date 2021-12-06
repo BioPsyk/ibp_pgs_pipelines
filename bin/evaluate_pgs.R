5b7a91c7190a9483d4be410161a31a0c42d76fc9
vaqm2 <appadurai.vivek@gmail.com>
2021-12-06 14:23:51 +0100

    check parser

--- bin/evaluate_pgs.R
+++ bin/evaluate_pgs.R
@@ -44,7 +44,7 @@ parser = add_argument(parser,
                       for liability transformation")
 options = parse_args(parser)
 
-if(isTRUE(options$binary) & !exists(options$prevalence)) {
+if(isTRUE(options$binary) & !exists("options$prevalence")) {
     print("ERROR: --binary flag implies binary trait and requires --prevalence")
     stop()
 }
