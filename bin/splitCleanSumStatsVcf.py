29004a6b8be337a87a0146d6ca33d6acd4bcda83
vaqm2 <appadurai.vivek@gmail.com>
2022-03-09 10:46:20 +0100

    In process of switching source to clean sumstats library

--- bin/splitCleanSumStatsVcf.py
+++ bin/splitCleanSumStatsVcf.py
@@ -7,13 +7,12 @@ from os import path
 import re
 import numpy
 
-
 def main():
     parser = argparse.ArgumentParser(description = """Splits a summary stats file in VCF 
     format by chromosome.
     Reformats to:
     mt-cojo format for sBayesR,
-    tab delimited format format for prs-cs""")
+    tab delimited format format for prs-c or prsice""")
     parser.add_argument('--chromosome', type = str, help = "Chromosome to split from VCF", required = True)
     parser.add_argument('--vcf', type = str, help = "Path to a gzipped VCF", required = True)
     parser.add_argument('--format', type = str, help = "prscs or sbayesr or prsice", required = True)
@@ -33,55 +32,55 @@ def main():
     elif(args.format == "prsice"):
         out_fh.write("SNP CHR BP A1 A2 BETA SE P\n")
     else:
-        sys.exit("FATAL: --format is invalid")
+        sys.exit("ERROR: Unsupported --format specified. Select either prscs or sbayesr or prsice!")
 
     if path.exists(args.vcf):
         gwas_vcf = VCF(args.vcf)
         for variant in gwas_vcf(args.chromosome):
-            alt_allele = ''.join(variant.ALT)
+            chromosome = variant.chromosome
+            position   = variant.start + 1
             ref_allele = variant.REF
+            alt_allele = ''.join(variant.ALT)
+            effect     = str(variant.format('ES').flat[0])
+            std_error  = str(variant.format('SE').flat[0])
+            p_value    = str(variant.format('EP').flat[0])
+            maf        = variant.format('AF').flat[0]
+            N          = str(args.N)
+
             if(len(alt_allele) > 1 or len(ref_allele) > 1): # Ignores INDELs in the input GWAS dataset
+                print("INFO: Ignoring INDEL/MULTI ALLELIC VARIANT at :" + snp + ":" + chromosome + " " + position)
                 continue
+
+            if (maf > 0.5):
+                maf = 1 - 0.5
+
+            maf = str(maf)
+
             if (variant.ID):
-                if variant.ID in snp_dict.keys():
+                snp = variant.ID
+                if snp in snp_dict.keys():
+                    print("INFO: Ignoring DUPLICATE VARIANT :" + snp + "\n")
                     continue
-                else:
-                    out_fh.write(variant.ID)
-                    out_fh.write(" ")
-                    snp_dict[variant.ID] = 1
             else:
-                out_fh.write(variant.CHROM)
-                out_fh.write("_")
-                out_fh.write(str(variant.start + 1)) # Positions returned by CyVCF2 API are 0-based
-                out_fh.write("_")
-                out_fh.write(ref_allele) 
-                out_fh.write("_")
-                out_fh.write(alt_allele)
-                out_fh.write(" ")
+                snp = chromosome + "_" + position + "_" + ref_allele + "_" + alt_allele
+
+            out_fh.write(snp + " ")
             if(args.format == 'prsice'):
-                out_fh.write(variant.CHROM)
-                out_fh.write(" ")
-                out_fh.write(str(variant.start + 1))
-                out_fh.write(" ")
-            out_fh.write(alt_allele)
-            out_fh.write(" ")
-            out_fh.write(ref_allele)
-            out_fh.write(" ")
+                out_fh.write(chromosome + " ")
+                out_fh.write(position + " ")
+            out_fh.write(alt_allele + " ")
+            out_fh.write(ref_allele + " ")
             if(args.format == "sbayesr"):
-                out_fh.write(str(variant.INFO.get('AF')))
-                out_fh.write(" ")
-            out_fh.write(str(variant.format('ES').flat[0]))
-            out_fh.write(" ")
+                out_fh.write(maf + " ")
+            out_fh.write(effect + " ")
             if (args.format == "sbayesr" or args.format == "prsice"):
-                out_fh.write(str(variant.format('SE').flat[0]))
-                out_fh.write(" ")
-            out_fh.write(str(pow(10, -1 * variant.format('LP').flat[0])))
-            out_fh.write(" ")
+                out_fh.write(std_error + " ")
+            out_fh.write(p_value + " ")
             if (args.format == "sbayesr"):
-                out_fh.write(str(args.n))
+                out_fh.write(N)
             out_fh.write("\n")
     else:
-        sys.exit("FATAL: File not found: " + args.vcf + "!!")
+        sys.exit("ERROR: File not found: " + args.vcf + "!!")
 
     out_fh.close()
 
